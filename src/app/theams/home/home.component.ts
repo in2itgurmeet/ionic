b33ef82bcd873d83de389c6fba08d912732cbf94
Gurmeet Prajapati <initgurmeet@gmail.com>
2026-03-23 14:59:29 +0530

    change slider and some content

--- src/app/theams/home/home.component.ts
+++ src/app/theams/home/home.component.ts
@@ -1,13 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { AuthRoutingModule } from "src/app/auth/auth-routing-module";
+import { register } from 'swiper/element/bundle';
+register();
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, IonicModule, AuthRoutingModule],
+  imports: [CommonModule, IonicModule, AuthRoutingModule,],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
 })
 export class HomeComponent implements OnInit {
 
